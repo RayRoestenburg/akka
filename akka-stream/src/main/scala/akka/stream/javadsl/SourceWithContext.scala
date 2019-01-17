b90a3987780159191891df1a8f8b6c46fa58c5bf
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-01-17 16:22:46 +0100

    Implementation cleanups of SourceWithContext

--- akka-stream/src/main/scala/akka/stream/javadsl/SourceWithContext.scala
+++ akka-stream/src/main/scala/akka/stream/javadsl/SourceWithContext.scala
@@ -18,59 +18,47 @@ import scala.compat.java8.FutureConverters._
  * API MAY CHANGE
  */
 @ApiMayChange
-object SourceWithContext {
-  def from[Out, Mat](underlying: Source[Out, Mat]): SourceWithContext[Out, Out, Mat] = {
-    new SourceWithContext(scaladsl.SourceWithContext(underlying.asScala))
-  }
-
-  def fromPairs[Out, Ctx, Mat](under: Source[Pair[Out, Ctx], Mat]): SourceWithContext[Ctx, Out, Mat] = {
-    new SourceWithContext(scaladsl.SourceWithContext.from(under.asScala.map(_.toScala)))
-  }
-}
-
-/**
- * API MAY CHANGE
- */
-@ApiMayChange
 final class SourceWithContext[+Ctx, +Out, +Mat](delegate: scaladsl.SourceWithContext[Ctx, Out, Mat]) extends GraphDelegate(delegate) {
-  def mapContext[Ctx2](extractContext: function.Function[Ctx, Ctx2]): SourceWithContext[Ctx2, Out, Mat] = {
-    new SourceWithContext(delegate.mapContext(extractContext.apply))
-  }
-
-  def via[Ctx2, Out2, Mat2](viaFlow: Graph[FlowShape[Pair[Out @uncheckedVariance, Ctx @uncheckedVariance], Pair[Out2, Ctx2]], Mat2]): SourceWithContext[Ctx2, Out2, Mat] = {
-    val under = endContextPropagation().via(viaFlow)
-    SourceWithContext.fromPairs(under)
-  }
+  def via[Ctx2, Out2, Mat2](viaFlow: Graph[FlowShape[Pair[Out @uncheckedVariance, Ctx @uncheckedVariance], Pair[Out2, Ctx2]], Mat2]): SourceWithContext[Ctx2, Out2, Mat] =
+    viaScala(_.via(akka.stream.scaladsl.Flow[(Out, Ctx)].map { case (o, c) ⇒ Pair(o, c) }.via(viaFlow).map(_.toScala)))
 
   def endContextPropagation(): Source[Pair[Out @uncheckedVariance, Ctx @uncheckedVariance], Mat @uncheckedVariance] =
     delegate.endContextPropagation.map { case (o, c) ⇒ Pair(o, c) }.asJava
 
-  def map[Out2](f: function.Function[Out, Out2]): SourceWithContext[Ctx, Out2, Mat] =
-    new SourceWithContext(delegate.map(f.apply))
-
-  def mapAsync[Out2](parallelism: Int, f: function.Function[Out, CompletionStage[Out2]]): SourceWithContext[Ctx, Out2, Mat] =
-    new SourceWithContext(delegate.mapAsync[Out2](parallelism)(o ⇒ f.apply(o).toScala))
+  // operations in alphabetic order
 
   def collect[Out2](pf: PartialFunction[Out, Out2]): SourceWithContext[Ctx, Out2, Mat] =
-    new SourceWithContext(delegate.collect(pf))
+    viaScala(_.collect(pf))
 
   def filter(p: function.Predicate[Out]): SourceWithContext[Ctx, Out, Mat] =
-    new SourceWithContext(delegate.filter(p.test))
+    viaScala(_.filter(p.test))
 
   def filterNot(p: function.Predicate[Out]): SourceWithContext[Ctx, Out, Mat] =
-    new SourceWithContext(delegate.filterNot(p.test))
+    viaScala(_.filterNot(p.test))
 
   def grouped(n: Int): SourceWithContext[java.util.List[Ctx @uncheckedVariance], java.util.List[Out @uncheckedVariance], Mat] =
-    new SourceWithContext(delegate.grouped(n).map(_.asJava)).mapContext(_.asJava)
+    viaScala(_.grouped(n).map(_.asJava).mapContext(_.asJava))
+
+  def map[Out2](f: function.Function[Out, Out2]): SourceWithContext[Ctx, Out2, Mat] =
+    viaScala(_.map(f.apply))
+
+  def mapAsync[Out2](parallelism: Int, f: function.Function[Out, CompletionStage[Out2]]): SourceWithContext[Ctx, Out2, Mat] =
+    viaScala(_.mapAsync[Out2](parallelism)(o ⇒ f.apply(o).toScala))
 
   def mapConcat[Out2](f: function.Function[Out, _ <: java.lang.Iterable[Out2]]): SourceWithContext[Ctx, Out2, Mat] =
-    new SourceWithContext(delegate.mapConcat(elem ⇒ Util.immutableSeq(f.apply(elem))))
+    viaScala(_.mapConcat(elem ⇒ Util.immutableSeq(f.apply(elem))))
+
+  def mapContext[Ctx2](extractContext: function.Function[Ctx, Ctx2]): SourceWithContext[Ctx2, Out, Mat] =
+    viaScala(_.mapContext(extractContext.apply))
 
   def statefulMapConcat[Out2](f: function.Creator[function.Function[Out, java.lang.Iterable[Out2]]]): SourceWithContext[Ctx, Out2, Mat] =
-    new SourceWithContext(delegate.statefulMapConcat { () ⇒
+    viaScala(_.statefulMapConcat { () ⇒
       val fun = f.create()
       elem ⇒ Util.immutableSeq(fun(elem))
     })
 
   def asScala: scaladsl.SourceWithContext[Ctx, Out, Mat] = delegate
+
+  private[this] def viaScala[Ctx2, Out2, Mat2](f: scaladsl.SourceWithContext[Ctx, Out, Mat] ⇒ scaladsl.SourceWithContext[Ctx2, Out2, Mat2]): SourceWithContext[Ctx2, Out2, Mat2] =
+    new SourceWithContext[Ctx2, Out2, Mat2](f(delegate))
 }
--- akka-stream/src/main/scala/akka/stream/scaladsl/SourceWithContext.scala
+++ akka-stream/src/main/scala/akka/stream/scaladsl/SourceWithContext.scala
@@ -10,34 +10,23 @@ import akka.annotation.ApiMayChange
 import akka.stream._
 
 /**
+ * A source that automatically propagates a context with an element.
+ *
+ * Can be created by calling [[Source.startContextPropagation()]]
+ *
  * API MAY CHANGE
  */
 @ApiMayChange
-object SourceWithContext {
-  def apply[Out, Mat](underlying: Source[Out, Mat]): SourceWithContext[Out, Out, Mat] = {
-    val under = underlying.map(e ⇒ (e, e))
-    new SourceWithContext[Out, Out, Mat](under)
-  }
-  def from[Out, Ctx, Mat](under: Source[(Out, Ctx), Mat]): SourceWithContext[Ctx, Out, Mat] = {
-    new SourceWithContext[Ctx, Out, Mat](under)
-  }
-}
-
-/**
- * API MAY CHANGE
- */
-@ApiMayChange
-final class SourceWithContext[+Ctx, +Out, +Mat](
+final class SourceWithContext[+Ctx, +Out, +Mat] private[stream] (
   delegate: Source[(Out, Ctx), Mat]
 ) extends GraphDelegate(delegate) with FlowWithContextOps[Ctx, Out, Mat] {
-
   override type ReprMat[+C, +O, +M] = SourceWithContext[C, O, M @uncheckedVariance]
 
   override def via[Ctx2, Out2, Mat2](viaFlow: Graph[FlowShape[(Out, Ctx), (Out2, Ctx2)], Mat2]): Repr[Ctx2, Out2] =
-    SourceWithContext.from(delegate.via(viaFlow))
+    new SourceWithContext(delegate.via(viaFlow))
 
   override def viaMat[Ctx2, Out2, Mat2, Mat3](flow: Graph[FlowShape[(Out, Ctx), (Out2, Ctx2)], Mat2])(combine: (Mat, Mat2) ⇒ Mat3): SourceWithContext[Ctx2, Out2, Mat3] =
-    SourceWithContext.from(delegate.viaMat(flow)(combine))
+    new SourceWithContext(delegate.viaMat(flow)(combine))
 
   /**
    * Stops automatic context propagation from here and converts this to a regular
